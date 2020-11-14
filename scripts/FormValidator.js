ecb41e8ed9fb4547735fb524287052e56acf1314
Katya Ksenofontova <dkurach@MacBook-Air-Dmitry.local>
2020-11-14 13:40:54 +0300

    feat:add class FormValidator

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -64,7 +64,7 @@ export class FormValidator {
     resetInputError() {
         this._inputList.forEach((inputElement) => {
             this._hideInputError(inputElement);
-            this._toggleButtonState(this._inputList, this._buttonElement);
+            this.disableButtonSubmit(this._inputList, this._buttonElement);
         })
     }
 }
