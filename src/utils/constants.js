a9f8077b0d608f4396c9f609f10ff9391f4300b9
Katya Ksenofontova <dkurach@MacBook-Air-Dmitry.local>
2020-12-08 19:07:57 +0300

    fix: popup_type_confirm

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,6 +9,7 @@ export const submitPopupProfileButton = document.querySelector("#save-profile")
 export const formProfile = document.querySelector("#form-profile");
 export const nameProfile = document.querySelector(".profile__name");
 export const professionProfile = document.querySelector(".profile__profession");
+export const avatarProfile = document.querySelector(".profile__avatar");
 export const nameProfileInput = document.querySelector("#name");
 export const professionProfileInput = document.querySelector("#profession");
 
@@ -17,12 +18,22 @@ export const openPopupGalleryButton = document.querySelector(".button_type_add-g
 export const submitPopupGalleryButton = document.querySelector("#save-gallery");
 export const formGallery = document.querySelector("#form-gallery");
 
-export const nameCardInput = document.querySelector("#name-card");
-export const linkCardInput = document.querySelector("#link-card");
+export const nameCardInput = document.querySelector("#place");
+export const linkCardInput = document.querySelector("#link");
 
 export const popupCard = document.querySelector(".popup_type_card");
 export const imagePopupCard = document.querySelector(".popup__image-card");
 export const titlePopupCard = document.querySelector(".popup__title_type_card");
+
+export const popupConfirm = document.querySelector(".popup_type_check");
+export const titlePopupConfirm = document.querySelector(".popup__title_type_check");
+export const submitPopupConfirmButton = document.querySelector("#save-check");
+
+export const popupAvatar = document.querySelector(".popup_type_avatar");
+export const openPopupAvatarButton = document.querySelector((".profile__avatar"));
+export const submitPopupAvatarButton = document.querySelector("#save-avatar");
+export const formAvatar = document.querySelector("#form-avatar");
+export const linkAvatarInput = document.querySelector("#link-avatar");
 
 export const configValidation = {
     formSelector: ".popup__form",
